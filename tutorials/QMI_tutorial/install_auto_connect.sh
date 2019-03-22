3d0f5ca5ff2bd2ada05fb3c7eaa992b56fb8f311
saeedjohar <saeed_johar@yahoo.com>
2019-03-22 13:12:47 +0300

    modified install script

--- tutorials/QMI_tutorial/install_auto_connect.sh
+++ tutorials/QMI_tutorial/install_auto_connect.sh
@@ -3,8 +3,8 @@
 echo "What is the APN?"
 read carrierapn
 
-cp reconnect_service qmi_reconnect.service
-cp reconnect_sh qmi_reconnect.sh
+wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/master/tutorials/QMI_tutorial/reconnect_service -O qmi_reconnect.service
+wget --no-check-certificate https://raw.githubusercontent.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/master/tutorials/QMI_tutorial/reconnect_sh -O qmi_reconnect.sh
 
 sed -i "s/#APN/$carrierapn/" qmi_reconnect.sh
 
