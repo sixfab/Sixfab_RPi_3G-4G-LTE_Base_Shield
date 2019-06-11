2ba002310683487988c3e509bda2978d7e90ab24
saeedjohar <saeed_johar@yahoo.com>
2019-06-11 17:01:06 +0300

    qmi_install.sh modified for 4.19 kernel

--- tutorials/QMI_tutorial/qmi_install.sh
+++ tutorials/QMI_tutorial/qmi_install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+: '
+QMI installation script by Sixfab
+
+Created By Metin Koc, Nov 2018
+Modified by Saeed Johar, 11th June 2019
+'
+
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 SET='\033[0m'
-
-	
 
 echo "${YELLOW}Clear Files${SET}"
 rm -rf /home/pi/files
@@ -14,14 +19,29 @@ echo "${YELLOW}Change directory to /home/pi${SET}"
 cd /home/pi
 
 echo "${YELLOW}Downloading source files${SET}"
-wget https://sixfab.com/wp-content/uploads/2018/11/files.zip
-unzip files.zip
+wget https://github.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/raw/master/tutorials/QMI_tutorial/src/quectel-CM.zip
+unzip quectel-CM.zip -d /home/pi/files/ && rm -r quectel-CM.zip
 
-echo "${YELLOW}Updating rpi${SET}"
-apt-get update
+
+#echo "${YELLOW}Updating rpi${SET}"
+#apt-get update
 
 #echo "${YELLOW}Downlading kernel headers${SET}"
 #apt-get install raspberrypi-kernel-headers
+echo "${YELLOW}Checking Kernel${SET}"
+
+case $(uname -r) in
+    4.14*) echo $(uname -r) based kernel found
+        echo "${YELLOW}Downloading source files${SET}"
+        wget https://github.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/raw/master/tutorials/QMI_tutorial/src/4.14.zip -O drivers.zip
+        unzip drivers.zip -d /home/pi/files/ && rm -r drivers.zip;;
+    4.19*) echo $(uname -r) based kernel found 
+        echo "${YELLOW}Downloading source files${SET}"
+        wget https://github.com/sixfab/Sixfab_RPi_3G-4G-LTE_Base_Shield/raw/master/tutorials/QMI_tutorial/src/4.19.1.zip -O drivers.zip
+        unzip drivers.zip -d /home/pi/files/ && rm -r drivers.zip;;
+    *) echo "Driver for $(uname -r) kernel not found";exit 1;
+
+esac
 
 echo "${YELLOW}Installing udhcpc${SET}"
 apt-get install udhcpc
@@ -33,15 +53,14 @@ chmod +x /usr/share/udhcpc/default.script
 
 echo "${YELLOW}Change directory to /home/pi/files/drivers${SET}"
 cd /home/pi/files/drivers
-make clean && make && make install
+make && make install
 
 echo "${YELLOW}Change directory to /home/pi/files/quectel-CM${SET}"
 cd /home/pi/files/quectel-CM
-make clean && make
+make
 
-echo "${YELLOW}After reboot please start following command${SET}"
+echo "${YELLOW}After reboot please follow commands mentioned below${SET}"
 echo "${YELLOW}go to /home/pi/files/quectel-CM and run sudo ./quectel-CM -s [YOUR APN]${SET}"
 
-echo "${YELLOW}Press any key to reboot!${SET}"
-read key
+read -p "Press ENTER key to reboot" ENTER
 reboot
